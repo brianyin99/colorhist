26eb2508561ca77fd32c6c0e53dcf4955c2128fc
brianyin99 <41968577+brianyin99@users.noreply.github.com>
2018-07-31 14:35:34 -0500

    Add MATLAB files

---
+++ figureground.m
@@ -0,0 +1,21 @@
+function [prediction] = figureground(rgbimage, segtrans_path)
+% Take image (imageheightximagewidthxRGB), return image segmented
+
+addpath(genpath(segtrans_path)) % path to segtrans folder (must be run from segtrans)
+
+%% Load the prepared training data of voc10.
+source = load_value('data/xps/voc10/train_source.mat');
+
+%% Segment a single image
+image = rgbimage; % cell of RGB uint8 images
+windows = st_windows(image,100);
+features = st_features_gist(image,windows);
+weights = ones(1,100)/100;
+mask = st_transfer(source,50,windows,features,@distance_L2,weights);
+prediction = st_segment(image,mask,0.2,20);
+
+%% Display the results.
+% show_prediction(prediction,image,mask);
+
+end
+
---
+++ segmenter_func.m
@@ -0,0 +1,42 @@
+function segmenter_func(myfolder, new_folder, segtrans_path)
+%{ 
+Take folder of images, segment using: http://calvin.inf.ed.ac.uk/software/figure-ground-segmentation-by-transferring-window-masks/
+
+MYFOLDER -- Absolute filepath to folder of images to segment
+NEWFOLDER -- Name of new folder (will appear in current MATLAB folder)
+SEGTRANS_PATH -- Absolute filepath to segtrans folder downloaded from website above.
+
+%}
+
+mkdir(new_folder)
+
+% loops through every image in myfolder
+myfiles = dir(myfolder);
+num_images = length(myfiles) - 2;
+for i = 3:num_images + 2
+    
+    % reverse-engineer the file path for each image
+    myfile = myfiles(i);
+    myname = myfile.name;
+    inputimage = strcat(myfolder,'/', myname);
+    
+    % for when myname is '.DS_Store' (info mac stores in folders)
+    if myname(2) == 'D'
+        continue
+    end
+    
+    % read-in rgb image
+    rgbimage = imread(inputimage);
+    
+    % takes care of indexed images
+    if class(rgbimage) == 'uint8'
+        [X, map] = imread(inputimage);
+        if ~isempty(map)
+            rgbimage = ind2rgb(X,map);
+        end
+    end
+    
+    % find prediction for figure-ground, imageheightximagewidth logical
+    prediction = figureground(rgbimage, segtrans_path);
+    imwrite(prediction, fullfile(new_folder, strcat(myname,'.png')))
+end
